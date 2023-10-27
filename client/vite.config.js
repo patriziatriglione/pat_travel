dd40075dbb423a966d2e9edd53e50fda940e7eeb
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-27 11:09:35 +0200

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +12,8 @@ export default defineConfig({
       },
     },
   },
-})
+  optimizeDeps: {
+    include: ['react-redux'], // Assicurati di includere 'react-redux' tra le dipendenze ottimizzate
+  },
+});
+
