99df7eeb3c44fbcbc606bbcb00c5c0cfc7bd5370
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-29 17:00:08 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://pat-travel-api.vercel.app',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
