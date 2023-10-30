acdae937860ca6c3b3b9fb228cc6919b4fd38072
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-30 10:04:32 +0100

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -48,6 +48,7 @@ const login = async (req, res) => {
         const { password, ...otherDetails } = user._doc;
         res
             .cookie("access_token", token, {
+                httpOnly: true,
                 secure: true,
             })
             .status(httpStatus.OK).json({ ...otherDetails })
