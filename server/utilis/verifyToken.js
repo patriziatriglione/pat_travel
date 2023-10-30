8bf44dc1cd4f5afca251e5a66de9605c1f6c4d40
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-30 07:44:58 +0100

    Update verifyToken.js

--- server/utilis/verifyToken.js
+++ server/utilis/verifyToken.js
@@ -14,23 +14,24 @@ const verifyToken = (req, res, next) => {
     })
 }
 const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, () => {
+    verifyToken(req, res, () => {
         if (req.user.id === req.params.id || req.user.isAdmin) {
             next();
         } else {
             return res.status(httpStatus.UNAUTHORIZED).send("You are not authorized!")
         }
-    })
-}
+    });
+};
+
 const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
+    verifyToken(req, res, () => {
         if (req.user.isAdmin) {
             next();
         } else {
             return res.status(httpStatus.UNAUTHORIZED).send("You are not authorized!")
         }
-    })
-}
+    });
+};
 
 module.exports = {
     verifyToken,
