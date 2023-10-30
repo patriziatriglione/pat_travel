9a4e180046f39a4c6e59c8bd7ee10b75a73e28df
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-30 09:29:05 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,44 +11,44 @@ const orderRoutes = require("./routes/order")
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
 
-
 // function for dotenv
 dotenv.config();
+
 // function for express
 const app = express();
-app.use(cors(
-    {
-        origin:["https://pat-travel.vercel.app"],
-        methods:["POST", "GET", "DELETE", "PATCH"],
-        credentials: true
-    }
-))
-app.use(express.urlencoded({ extended: true }));
-//cookie
-app.use(cookieParser())
-// to use json method
-app.use(express.json());
-//PORT
-const port = 3000;
-// connect to mongoose
-mongoose.connect(process.env.KEY_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-mongoose.connection.on("error", err => {
-    console.log(err)
-})
-mongoose.connection.on("connected", res => {
-    console.log("connected")
-})
+
+// Aggiungi middleware CORS e configurazione delle sessioni prima delle tue rotte
+app.use(cors({
+  origin: ["https://pat-travel.vercel.app"],
+  methods: ["POST", "GET", "DELETE", "PATCH"],
+  credentials: true
+}));
+
+app.use(cookieParser());
+
+// Il codice per le sessioni e altre configurazioni dovrebbe andare qui...
+
+// Le tue rotte dovrebbero andare dopo queste configurazioni
 app.use("/api/package", packageRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/structure", structureRoutes);
 app.use("/api/activity", activityRoutes);
 app.use("/api/food", foodRoutes);
-app.use("/api/order", orderRoutes)
+app.use("/api/order", orderRoutes);
 
+// Il tuo server dovrebbe rimanere uguale
+const port = 3000;
+mongoose.connect(process.env.KEY_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("error", err => {
+  console.log(err)
+});
+mongoose.connection.on("connected", res => {
+  console.log("connected")
+});
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`)
+});
