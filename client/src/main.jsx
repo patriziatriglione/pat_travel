cbf8a45cdad763bbe706b26501519706e1d935ac
patriziatriglione <patrizia.triglione@gmail.com>
2023-10-31 07:54:35 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 import { Provider } from 'react-redux';
@@ -7,13 +6,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { StrictMode } from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
